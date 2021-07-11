e3c84164052494d4987461681273b56154e5eb32
rihabcheberli <rihabcheberli@insat.u-carthage.tn>
2021-07-11 16:21:04 +0100

    products data

---
+++ src/components/Products/data.js
@@ -0,0 +1,63 @@
+import product1 from '../../images/product1.png' ;
+import product2 from '../../images/product2.png' ;
+import product3 from '../../images/product3.png' ;
+import product4 from '../../images/product4.png' ;
+import product5 from '../../images/product5.png' ;
+import product6 from '../../images/product6.png' ;
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Knitwear',
+        name: 'Men knitwear offers',
+        desc:
+            '',
+        price: '$19.99',
+        button: 'Buy now'
+    },
+    {
+        img: product2,
+        alt: 'Shirts',
+        name: 'Men shirts offers',
+        desc:
+            '',
+        price: '$4,99',
+        button: 'Buy now'
+    },
+    {
+        img: product3,
+        alt: 'T-shirts',
+        name: 'Various T-shorts',
+        desc:
+            '',
+        price: '$14.99',
+        button: 'Buy now'
+    },
+    {
+        img: product4,
+        alt: 'T-shirts',
+        name: 'Novelty T-shirts',
+        desc:
+            '',
+        price: '$14.99',
+        button: 'Buy now'
+    },
+    {
+        img: product5,
+        alt: 'T-shirts',
+        name: 'Men Casual Lizard',
+        desc:
+            '',
+        price: '$14.99',
+        button: 'Buy now'
+    },
+    {
+        img: product6,
+        alt: 'T-shirts',
+        name: 'Pull&Bear Jumper',
+        desc:
+            '',
+        price: '$14.99',
+        button: 'Buy now'
+    }
+];
